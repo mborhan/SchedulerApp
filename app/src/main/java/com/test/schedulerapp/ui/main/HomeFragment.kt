34a2cc91429df1c9eec88d6aa1e4fa2edff8b306
borhan uddin <borhan2656@gmail.com>
2025-09-07 15:21:43 +0600

    Modified Home page UI.
    Signed-off-by: borhan uddin <borhan2656@gmail.com>

--- app/src/main/java/com/test/schedulerapp/ui/main/HomeFragment.kt
+++ app/src/main/java/com/test/schedulerapp/ui/main/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.test.schedulerapp.ui.main
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.test.schedulerapp.R
@@ -11,7 +12,7 @@ import com.test.schedulerapp.ui.applist.AppListFragment
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val button = view.findViewById<Button>(R.id.goToDetailButton)
+        val button = view.findViewById<TextView>(R.id.show_install_app)
         button.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, AppListFragment())
